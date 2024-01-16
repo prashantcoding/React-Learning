88039e403e2d981c6409cc8dddb722a6821acc09
prashantcoding <soultaker399@gmail.com>
2024-01-16 23:18:39 +0530

    sorting table

--- src/components/columns.js
+++ src/components/columns.js
@@ -30,3 +30,44 @@ export const Columns = [
     Footer: "Phone",
   },
 ];
+
+export const Gropu_Columns = [
+  { Header: "Id", accessor: "id", Footer: "Id" },
+  {
+    Header: "Name",
+    Footer: "Name",
+    columns: [
+      {
+        Header: "first Name",
+        accessor: "first_name",
+        Footer: "first Name",
+      },
+      {
+        Header: "Last Name",
+        accessor: "last_name",
+        Footer: "Last Name",
+      },
+    ],
+  },
+  {
+    Header: "Info",
+    Footer: "Info",
+    columns: [
+      {
+        Header: "Date of Birth",
+        accessor: "data_of_birth",
+        Footer: "Date of Birth",
+      },
+      {
+        Header: "Country",
+        accessor: "country",
+        Footer: "Country",
+      },
+      {
+        Header: "Phone",
+        accessor: "phone",
+        Footer: "Phone",
+      },
+    ],
+  },
+];
